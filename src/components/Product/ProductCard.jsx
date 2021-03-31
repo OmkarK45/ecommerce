d8b164f368cab38376fa951ae2af4697f9707a42
Omkar Kulkarni <omkarak45@gmail.com>
2021-03-31 17:50:10 +0530

    feat:Added heart icon with state

--- src/components/Product/ProductCard.jsx
+++ src/components/Product/ProductCard.jsx
@@ -1,20 +1,48 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-no-bind */
 import Button from "components/ui/Button/Button"
-import { HiOutlineShoppingBag, HiOutlineShoppingCart } from "react-icons/hi"
+import {
+  HiOutlineShoppingBag,
+  HiOutlineShoppingCart,
+  HiHeart,
+  HiOutlineHeart,
+} from "react-icons/hi"
 import toast from "react-hot-toast"
 import { useCart } from "../../context/cartContext"
 import { handleAddToCart } from "./../../context/actions/cartActions"
+import {
+  handleAddToWishList,
+  handleRemoveFromWishList,
+} from "./../../context/actions/wishListActions"
 
 export default function ProductCard({ product }) {
-  const { dispatch: cartDispatch } = useCart()
+  const { state: cart, dispatch: cartDispatch } = useCart()
   const notify = (message) => toast.success(message)
+
   function addToCart(product) {
-    console.log(product.id)
     cartDispatch(handleAddToCart(product))
     notify("Product Added to Cart")
   }
+  const wishListed = cart.wishList.filter((item) => item.id === product.id)
+
+  function addToWishlist(product) {
+    if (wishListed.length === 0) {
+      cartDispatch(handleAddToWishList(product))
+      notify("Item added to wishlist")
+    } else {
+      cartDispatch(handleRemoveFromWishList(product))
+      notify("Item removed from wishlist")
+    }
+  }
+
   return (
-    <div className="flex flex-col justify-between shadow rounded overflow-hidden w-full sm:w-48 md:w-56 bg-white  pb-4 justify-self-start">
+    <div className="flex flex-col justify-between shadow rounded overflow-hidden w-full sm:w-48 md:w-56 bg-white relative pb-4 justify-self-start">
+      <button
+        className="absolute right-4 rounded-full top-3 bg-red-100 p-1  text-red-500 text-lg transform  focus:outline-none hover:scale-150 transition"
+        onClick={() => addToWishlist(product)}
+      >
+        {wishListed.length === 0 ? <HiOutlineHeart /> : <HiHeart />}
+      </button>
       <img
         src={product.image}
         className="h-36 w-full object-contain block"
@@ -33,9 +61,9 @@ export default function ProductCard({ product }) {
         </div>
       </div>
       <div className="flex flex-col  space-y-2 space-y-2 mt-2 px-4">
-        <Button variant="primary" size="sm">
-          <HiOutlineShoppingBag className="mr-2 h-4 w-4" /> Buy Now
-        </Button>
+        {/* <Button variant="primary" size="sm">
+          <HiOutlineShoppingBag className="mr-2 h-4 w-4" /> Wishlist
+        </Button> */}
         <Button onClick={() => addToCart(product)} size="sm">
           <HiOutlineShoppingCart className="mr-2 h-4 w-4" />
           Add To Cart
