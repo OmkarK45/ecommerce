1d37320466efcda7faa94165671c277c46f15645
Omkar Kulkarni <omkarak45@gmail.com>
2021-04-05 17:36:41 +0530

    fix:holy jit was not enabled, use react-responsive-carousel instead of swiper

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,3 +1,25 @@
-export default function Carosel() {
-  return "TBD"
+import "react-responsive-carousel/lib/styles/carousel.min.css" // requires a
+import { Carousel } from "react-responsive-carousel"
+export default function Slideshow() {
+  return (
+    <Carousel
+      showThumbs={false}
+      showStatus={false}
+      infiniteLoop
+      autoPlay
+      emulateTouch
+      swipeable
+      dynamicHeight
+    >
+      <div className="h-[918px]">
+        <img src="https://unsplash.it/400/200" className="opacity-[35]" />
+      </div>
+      <div>
+        <img src="https://unsplash.it/400/200" />
+      </div>
+      <div>
+        <img src="https://unsplash.it/400/200" />
+      </div>
+    </Carousel>
+  )
 }
--- src/pages/Marketing.jsx
+++ src/pages/Marketing.jsx
@@ -1,3 +1,10 @@
+import Slideshow from "components/Carousel/Carousel"
+
 export default function Marketing() {
-  return <div>boo</div>
+  return (
+    <>
+      <Slideshow />
+      <button className="bg-[#F7F700]">TEST</button>
+    </>
+  )
 }
